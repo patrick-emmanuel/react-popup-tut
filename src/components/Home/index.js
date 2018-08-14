d089e3af7809f0c4c153cab43aa0a2ef14b2f0ec
Patrick Emmanuel <emmanuelpatrick25@gmail.com>
2018-08-14 18:30:39 +0100

    Add heading to the home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,6 +4,7 @@ import './styles.css';
 
 const Home = () => (
     <div className="wrapper">
+        <h2>Contact me to get your pizza.</h2>
         <Link className="contact" to="/contact">Contact me!</Link>
     </div>
 )
