dd46a0f67a7a8076268650562bc684d86183cfd3
Patrick Emmanuel <emmanuelpatrick25@gmail.com>
2018-08-15 10:39:17 +0100

    update route

--- src/components/Success/index.js
+++ src/components/Success/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import pizzaImage from './pizza.jpg';
 import './styles.css';
 
 const Home = () => (
     <div className="wrapper">
-        <h2>Thanks for your order.</h2>
+        <h2>Thank you for your order.</h2>
         <Link className="back" to="/">Back home!</Link>
     </div>
 )
